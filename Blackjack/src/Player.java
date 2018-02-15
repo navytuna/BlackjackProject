ace9bfcbd122c3e580479166543b1492c5725c79
Alex Hildebert <navytuna@users.noreply.github.com>
2018-02-15 17:27:07 -0500

    wip code
    I honestly don't expect this to work, but it gets me started with the rest of the code.

--- Blackjack/src/Player.java
+++ Blackjack/src/Player.java
@@ -1,11 +1,26 @@
+import java.util.Scanner;
 
 public class Player extends Hand{
-	private Hand hand = new Hand();
+	private int Cash;
 	
 	public Player() {
-		hand.drawCard();
-		hand.drawCard();
+		Cash = 1500;
 	}
 	
+	public void bet(){
+		System.out.println("\nHow much do you want to bet?");
+		//System.out.println("You currently have up to " + sTotalMoney() + " to bet.";
+		Scanner sc = new Scanner(System.in);
+		int bet = Integer.parseint(sc.nextLine()); //Probably broken
+		
+	}
 	
+	public void playerTurn(){
+		Hand hand = new Hand();
+		hand.drawCard();
+		hand.drawCard();
+		System.out.print("\n" + hand.toString() + " " + hand.toString(1));
+		bet();
+		
+	}
 }
