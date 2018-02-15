68a4b1fd95af87a2fefab38e761887550f163d9c
Alex Hildebert <navytuna@users.noreply.github.com>
2018-02-15 12:13:30 -0500

    Commenting out Tests
    Commenting or deleting all of the code that I used to test the hand funcionality

--- Blackjack/src/Blackjack.java
+++ Blackjack/src/Blackjack.java
@@ -2,11 +2,9 @@
 public class Blackjack extends Deck{
 	
 	public static Deck deck = new Deck();
-	private static Hand handTest = new Hand(-1);
 	
 	public static void main(String[] args) {
 		deck.shuffle();
-		HandTest();
 	}
 	
 	//Make sure to only use one deck ever
@@ -18,13 +16,13 @@ public class Blackjack extends Deck{
 //		}
 //	}
 	
-	public static void HandTest() {
-		handTest.drawCard();
-		System.out.println(handTest.toString());
-		handTest.drawCard();
-		System.out.println(handTest.toString(1));
-		handTest.drawCard();
-		System.out.println(handTest.toString(2));
-	}
+//	public static void HandTest() {
+//		handTest.drawCard();
+//		System.out.println(handTest.toString());
+//		handTest.drawCard();
+//		System.out.println(handTest.toString(1));
+//		handTest.drawCard();
+//		System.out.println(handTest.toString(2));
+//	}
 	
 }
